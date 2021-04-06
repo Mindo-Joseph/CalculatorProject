d491061679f2bd4b79f60bc67a5ade4c78629175
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2021-04-06 19:00:07 +0300

    Implement app as stateful component and add handleclick functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-const App = () => (
-  <>
-    <Display />
-    <ButtonPanel />
-  </>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick = (buttonName) => {
+    const newState = calculate(this.state, buttonName);
+    this.setState(newState);
+  };
+
+  render() {
+    const { total, next, operation } = this.state;
+    return (
+      <div>
+        <Display total={total} next={next} operation={operation} />
+        <ButtonPanel data={this.state} clickHandler={this.handleClick} />
+      </div>
+    );
+  }
+}
 export default App;
